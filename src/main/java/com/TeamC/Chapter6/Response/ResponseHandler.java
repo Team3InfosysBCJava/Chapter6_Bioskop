dfbb61577967c2a9726bdfcd45d8c88e98377cda
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-07-19 06:01:35 +0700

    push 4

--- src/main/java/com/TeamC/Chapter6/Response/ResponseHandler.java
+++ src/main/java/com/TeamC/Chapter6/Response/ResponseHandler.java
@@ -1,4 +1,13 @@
 package com.TeamC.Chapter6.Response;
 
+import com.TeamC.Chapter6.Model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public class ResponseHandler {
+    public static ResponseEntity<Object> generateResponse(String s, HttpStatus ok, List<User> result) {
+        return null;
+    }
 }
